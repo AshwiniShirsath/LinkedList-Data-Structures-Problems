3daafdeb4bfd9ab43c7e2fdd46234557fa4c44d6
AshwiniShirsath <shirsathashwini9495@gmail.com>
2022-04-21 23:55:11 +0530

    [ADDED] : Added First Node 70,30,56 in Linked List Node

---
+++ src/com/bridgelabz/linkedlistdatastructure/LinkedListDemo.java
@@ -0,0 +1,62 @@
+package com.bridgelabz.linkedlistdatastructure;
+/*
+ * @author : Ashwini 
+ * Ability to create Linked List by adding 30 and 56 to 70
+ */
+class Node {
+    public int data;
+    public Node next;
+
+    //Node Constructor for Creating New Node
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+    Node tail;
+    //Insert Data in New Node
+    public void insertFirst(int data) {
+        Node newNode = new Node(data);
+        //Adding Data in Node
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    //Display Node in Linked List
+    public void showLinkedList() {
+        if(head == null) {
+            System.out.println("Linked List is Empty.");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        //Welcome Message
+        System.out.println("Welcome to LinkedList Data Structures Problem.");
+
+        LinkedList linkedList = new LinkedList();
+
+        //Insert 70, 50, 36 in Linked List Nodes
+        linkedList.insertFirst(70);
+        linkedList.insertFirst(30);
+        linkedList.insertFirst(56);
+
+        //Showing Linked List Data
+        linkedList.showLinkedList();
+    }
+}
